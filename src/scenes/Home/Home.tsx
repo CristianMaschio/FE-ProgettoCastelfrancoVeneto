53ea466a29da448bf17dcc486e7431fa67cded9a
Cristian <blackspartan@hotmail.it>
2018-05-25 22:55:24 +0200

    riaggiunta la mappa

--- src/scenes/Home/Home.tsx
+++ src/scenes/Home/Home.tsx
@@ -1,5 +1,6 @@
 import "./Home.css";
 import * as React from "react";
+import Map from "../../components/Map/Map";
 import { Place } from "../../services/models/Place";
 import { getPlaces } from "../../services/api/places";
 
@@ -27,19 +28,14 @@ export default class Home extends React.Component<Props, State> {
   render() {
     return (
       <div className="home">
-        <h1>NearToMe</h1>
-      </div>
-    );
-  }
-}
-
-/*
-import Map from "../../components/Map/Map";
-
+        <h2>NearToMe</h2>
         <Map
           center={{ lat: 45.6716977, lng: 11.9265608 }}
           containerElement={<div style={{ height: `400px` }} />}
           mapElement={<div style={{ height: `100%` }} />}
           places={this.state.places}
         />
- */
+      </div>
+    );
+  }
+}
