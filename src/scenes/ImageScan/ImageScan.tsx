d5f4cb9c4b8d9a4e16489dfdca6bba369c042bb3
Niccolò Fanton <niccolofanton1997@gmail.com>
2018-05-26 05:49:20 +0200

    test

--- src/scenes/ImageScan/ImageScan.tsx
+++ src/scenes/ImageScan/ImageScan.tsx
@@ -15,11 +15,21 @@ export default class ImageScan extends React.PureComponent<Props, State> {
   // private webcam = null;
 
   constructor(props) {
+
     super(props);
 
     this.state = {
       webcamSwitch: true
     };
+
+    const video: HTMLVideoElement = document.getElementById("v") as HTMLVideoElement;
+    navigator.mediaDevices.enumerateDevices().then(res => {
+      console.log(res);
+      navigator.getUserMedia({ video: { deviceId: res[5].deviceId } }, (stream) => {
+        video.src = window.URL.createObjectURL(stream);
+      }, function(err) { alert("there was an error " + err); });
+
+    });
 
     this.capture = this.capture.bind(this);
   }
@@ -32,55 +42,51 @@ export default class ImageScan extends React.PureComponent<Props, State> {
 
     this.setState({ webcamSwitch: false });
 
-    navigator.getUserMedia({ video: true }, function(stream) {
-      const video: HTMLVideoElement = document.getElementById("v") as HTMLVideoElement;
-      const canvas: HTMLCanvasElement = document.getElementById("c") as HTMLCanvasElement;
-      video.src = window.URL.createObjectURL(stream);
+    const video: HTMLVideoElement = document.getElementById("v") as HTMLVideoElement;
+    const canvas: HTMLCanvasElement = document.getElementById("c") as HTMLCanvasElement;
 
-      canvas.getContext("2d").drawImage(video, 0, 0, 300, 300, 0, 0, 300, 300);
-      const imageSrc = canvas.toDataURL("image/png");
+    canvas.getContext("2d").drawImage(video, 0, 0, 300, 300, 0, 0, 300, 300);
+    const imageSrc = canvas.toDataURL("image/png");
 
-      const BASE64_MARKER = ';base64,';
-      if (imageSrc.indexOf(BASE64_MARKER) === -1) {
-        const parts = imageSrc.split(',');
-        const contentType = parts[0].split(':')[1];
-        const raw = decodeURIComponent(parts[1]);
-        return new Blob([raw], { type: contentType });
+    const BASE64_MARKER = ';base64,';
+    if (imageSrc.indexOf(BASE64_MARKER) === -1) {
+      const parts = imageSrc.split(',');
+      const contentType = parts[0].split(':')[1];
+      const raw = decodeURIComponent(parts[1]);
+      return new Blob([raw], { type: contentType });
+    }
+
+    const _parts = imageSrc.split(BASE64_MARKER);
+    const _contentType = _parts[0].split(':')[1];
+    const _raw = window.atob(_parts[1]);
+    const rawLength = _raw.length;
+
+    const uInt8Array = new Uint8Array(rawLength);
+
+    for (let i = 0; i < rawLength; ++i) {
+      uInt8Array[i] = _raw.charCodeAt(i);
+    }
+
+    return fetch('https://southcentralus.api.cognitive.microsoft.com/customvision/v2.0/Prediction/5e20bca9-cbf2-482f-9d16-123ad906c1f9/image?iterationId=e9c0e087-f1fc-4cb5-8b8c-82d7eca709c8', {
+      method: 'POST',
+      mode: 'CORS',
+      body: new Blob([uInt8Array], { type: _contentType }),
+      headers: {
+        'Content-Type': 'application/octet-stream',
+        'Prediction-Key': '156e4ef9a72e46c5a103c17691463b0f',
       }
-
-      const _parts = imageSrc.split(BASE64_MARKER);
-      const _contentType = _parts[0].split(':')[1];
-      const _raw = window.atob(_parts[1]);
-      const rawLength = _raw.length;
-
-      const uInt8Array = new Uint8Array(rawLength);
-
-      for (let i = 0; i < rawLength; ++i) {
-        uInt8Array[i] = _raw.charCodeAt(i);
-      }
-
-      return fetch('https://southcentralus.api.cognitive.microsoft.com/customvision/v2.0/Prediction/5e20bca9-cbf2-482f-9d16-123ad906c1f9/image?iterationId=e9c0e087-f1fc-4cb5-8b8c-82d7eca709c8', {
-        method: 'POST',
-        mode: 'CORS',
-        body: new Blob([uInt8Array], { type: _contentType }),
-        headers: {
-          'Content-Type': 'application/octet-stream',
-          'Prediction-Key': '156e4ef9a72e46c5a103c17691463b0f',
+    })
+      .then(res => {
+        console.log(res);
+        return res.json();
+      })
+      .then(json => {
+        console.log(json);
+        if (json.predictions.length > 0) {
+          alert(json.predictions[0].tagName);
         }
       })
-        .then(res => {
-          console.log(res);
-          return res.json();
-        })
-        .then(json => {
-          console.log(json);
-          if (json.predictions.length > 0) {
-            alert(json.predictions[0].tagName);
-          }
-        })
-        .catch(err => err);
-
-    }, function(err) { alert("there was an error " + err); });
+      .catch(err => err);
 
   }
 
