048135f334c38daa4f3e918c07d4fdbd28f37652
Cristian <blackspartan@hotmail.it>
2018-05-26 02:42:25 +0200

    Ora l'header è funzionante

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Switch, Route, withRouter } from "react-router-dom";
 
+import Header from "./components/Header/Header";
 import Home from "./scenes/Home/Home";
 import Lab from "./scenes/Lab/Lab";
 import ItemDetails from "./scenes/ItemDetails/ItemDetails";
@@ -8,12 +9,14 @@ import ItemDetails from "./scenes/ItemDetails/ItemDetails";
 class App extends React.Component {
   public render() {
     return (
-      
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/Lab" exact component={Lab} />
-        <Route path="/Item/:itemId" exact component={ItemDetails} />
-      </Switch>
+      <>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/Lab" exact component={Lab} />
+          <Route path="/Item/:itemId" exact component={ItemDetails} />
+        </Switch>
+      </>
     );
   }
 }
